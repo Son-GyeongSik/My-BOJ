15052216fb91844b5d92c1ecaaae3bc86434a12a
GyeongSik Son <47748085+Son-GyeongSik@users.noreply.github.com>
2021-01-06 23:21:05 +0900

    Create 6549.cpp

---
+++ 6000번대/6500~6599/6549.cpp
@@ -0,0 +1,62 @@
+#include <iostream>
+#include <stack>
+#include <stdio.h>
+using namespace std;
+int main()
+{
+    int n;
+    while(1)
+    {
+    	cin>>n;
+    	if(n==0)
+    		break;
+    	long long st1[100001]={0,};
+		long long st2[100001]={0,};
+		long long q1[100001]={0,},in[100001]={0,},q2[100001]={0,};
+	    cin>>in[1];
+    	int cnt1=1;
+		long long a;
+		st1[cnt1]=in[1];
+    	st2[cnt1]=1;
+    	cnt1++;
+    	q1[1]=1;
+    	for(int i=2; i<=n; i++)
+    	{
+        	scanf("%d",&in[i]);
+        	while(cnt1>1 && in[i]<=st1[cnt1-1])
+            	cnt1--;
+        	if(cnt1==1)
+            	q1[i]=1;
+        	else
+            	q1[i]=st2[cnt1-1]+1;
+        	st1[cnt1]=in[i];
+        	st2[cnt1]=i;
+        	cnt1++;
+    	}
+    	cnt1=1;
+    	st1[cnt1]=in[n];
+    	st2[cnt1]=n;
+    	cnt1++;
+    	q2[n]=n;
+    	for(int i=n-1; i>=1; i--)
+    	{
+        	while(cnt1>1 && in[i]<=st1[cnt1-1])
+            	cnt1--;
+        	if(cnt1==1)
+            	q2[i]=n;
+        	else
+            	q2[i]=st2[cnt1-1]-1;
+        	st1[cnt1]=in[i];
+        	st2[cnt1]=i;
+        	cnt1++;
+    	}
+    	long long maxx=0;
+    	for(int i=1; i<=n; i++)
+    	{
+        	if(in[i]*(q2[i]-q1[i]+1)>maxx)
+            	maxx=in[i]*(q2[i]-q1[i]+1);
+    	}
+        cout<<maxx<<endl;
+    }
+    return 0;
+}
