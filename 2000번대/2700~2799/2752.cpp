6419b6336ec59b51f76bc2add767409d95e4e00d
GyeongSik Son <47748085+Son-GyeongSik@users.noreply.github.com>
2021-01-07 23:02:49 +0900

    Create 2752.cpp

---
+++ 2000번대/2700~2799/2752.cpp
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<iostream>
+#include<algorithm>
+using namespace std;
+int main()
+{
+	int arr[3];
+	cin>>arr[0]>>arr[1]>>arr[2];
+	sort(arr,arr+3);
+	cout<<arr[0]<<" "<<arr[1]<<" "<<arr[2]<<endl;
+	return 0;
+}	
