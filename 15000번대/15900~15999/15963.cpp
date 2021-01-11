9f6c2d5f2617df78b9cde35a3256ea71dc8023fb
GyeongSik Son <47748085+Son-GyeongSik@users.noreply.github.com>
2021-01-11 23:27:12 +0900

    Create 15963.cpp

---
+++ 15000번대/15900~15999/15963.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <stdio.h>
+#include <math.h>
+using namespace std;
+int main() {
+	long long a,b;
+	cin>>a>>b;
+	if(a==b)
+		cout<<"1"<<endl;
+	else
+		cout<<"0"<<endl;
+	return 0;
+}
