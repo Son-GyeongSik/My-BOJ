77ce93a8a5670e496d81500f556b02074a922ce3
GyeongSik Son <47748085+Son-GyeongSik@users.noreply.github.com>
2021-01-11 23:28:22 +0900

    Create 15680.cpp

---
+++ 15000번대/15600~15699/15680.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <stdio.h>
+using namespace std;
+
+int main() {
+	int a;
+	cin>>a;
+	if(a)
+		cout<<"Leading the Way to the Future"<<endl;
+	else
+		cout<<"YONSEI"<<endl;
+	return 0;
+}
