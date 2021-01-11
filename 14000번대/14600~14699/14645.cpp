63bea591257907651b94397e22317c7a64930a59
GyeongSik Son <47748085+Son-GyeongSik@users.noreply.github.com>
2021-01-11 23:36:51 +0900

    Create 14645.cpp

---
+++ 14000번대/14600~14699/14645.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <stdio.h>
+using namespace std;
+
+int main() {
+	int n,k;
+	int a,b;
+	for(int i=1; i<=n; i++)
+	{
+		scanf("%d %d",&a,&b);
+	}
+	cout<<"비와이"<<endl;
+	return 0;
+}
