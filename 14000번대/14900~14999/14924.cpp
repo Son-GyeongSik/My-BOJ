395c418dc07704ecfb77ece207c3b261ce2f1675
GyeongSik Son <47748085+Son-GyeongSik@users.noreply.github.com>
2021-01-12 23:37:59 +0900

    Create 14924.cpp

---
+++ 14000번대/14900~14999/14924.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <stdio.h>
+#include <algorithm>
+using namespace std;
+int main() {
+	int s,t,d;
+	int a;
+	cin>>s>>t>>d;
+	a=d/(s*2);
+	cout<<a*t<<endl;
+	return 0;
+}
