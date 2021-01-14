8352b02f922894349a60ab8f8e4bcc8a2ad742c3
GyeongSik Son <47748085+Son-GyeongSik@users.noreply.github.com>
2021-01-14 23:44:15 +0900

    Create 14264.cpp

---
+++ 14000번대/14200~14299/14264.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <stdio.h>
+#include <algorithm>
+#include <math.h>
+using namespace std;
+int main() {
+	double n;
+	cin>>n;
+	printf("%.13f\n",(n/2)*(n/2)*sqrt(3));
+	return 0;
+}
