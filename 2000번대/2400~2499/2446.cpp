661093ad1db10aa3b45f9f13f69b687609a5079d
GyeongSik Son <47748085+Son-GyeongSik@users.noreply.github.com>
2021-01-17 19:41:27 +0900

    Create 2446.cpp

---
+++ 2000번대/2400~2499/2446.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+int main() {
+	int n;
+	cin>>n;
+	for(int i=n; i>=1; i--)
+	{
+		for(int j=1; j<=n-i; j++)
+			cout<<" ";
+		for(int j=1; j<=(i*2-1); j++)
+			cout<<"*";
+		cout<<endl;
+	}
+	for(int i=2; i<=n; i++)
+	{
+		for(int j=1; j<=n-i; j++)
+			cout<<" ";
+		for(int j=1; j<=(i*2-1); j++)
+			cout<<"*";
+		cout<<endl;
+	}
+	return 0;
+}
